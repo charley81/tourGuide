8b4f135161413bc08fa892e710cfcb60c6b41785
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-15 11:33:02 -0500

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './components/loading'
 import Tours from './components/tours'
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
   return (
     <div className="App">
       <header>
