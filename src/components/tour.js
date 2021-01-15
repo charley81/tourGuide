9924b55e48146cf13ecd9633aaf4dd72be4247b5
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-15 11:27:17 -0500

    created header

---
+++ src/components/tour.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tour = () => {
+  return (
+    <div>
+      <h3>tour component</h3>
+    </div>
+  )
+}
+
+export default Tour
---
+++ src/components/tours.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <div>
+      <h3>tours component</h3>
+    </div>
+  )
+}
+
+export default Tours
